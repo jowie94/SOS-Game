8ef1df86fe9e4d29477a5cea333818c76d225aea
Joel Lopez <jowie.jowie@gmail.com>
2015-04-17 02:04:45 -0700

    General playing function

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@ type Score = (Int, Int)
 type Move = (Int, Int, Char)
 type GameBoard = Matrix Char
 
-data Game = Game GameBoard Score
+data GameState = Game GameBoard Score
 
 getLetter :: Int -> Char
 getLetter i
@@ -17,15 +17,28 @@ getLetter i
 
 -- TODO Function to get free spaces
 
+freeRowPositions :: [Char] -> Int -> [Int]
+freeRowPositions [] _ = []
+freeRowPositions (x:xs) n
+    | x == '_' = n:rest
+    | otherwise = rest
+    where rest = freeRowPositions xs (n+1)
+
+
+freePositions :: [[Char]] -> Int -> [(Int,Int)]
+freePositions [] _ = []
+freePositions (x:xs) n = (Prelude.concatMap (\y -> [(n, y)]) (freeRowPositions x 1)) ++ (freePositions xs $ n+1)
+
 randomDecision :: GameBoard -> IO Move
 randomDecision mat = do
     i <- getStdRandom (randomR (0::Int,1))
     let l = getLetter i
-    r <- getStdRandom (randomR (1, nrows mat))
-    c <- getStdRandom (randomR (1, ncols mat))
-    if (mat ! (r,c)) /= '_' then
-        randomDecision mat
-    else return (r, c, l)
+    --r <- getStdRandom (randomR (1, nrows mat))
+    --c <- getStdRandom (randomR (1, ncols mat))
+    let poss = freePositions (toLists mat) 1
+    el <- getStdRandom (randomR (0, (length poss) - 1))
+    let (r,c) = poss !! el
+    return (r, c, l)
 
 check :: GameBoard -> Char -> (Int, Int) -> (Int, Int) -> Int
 check mat 'O' pos@(r,c) (mx,my)
@@ -58,20 +71,18 @@ increaseScore (p1,p2) score player
     | player == 0 = (p1 + score, p2)
     | otherwise = (p1, p2 + score)
 
--- TODO extract to a general function
-
-randomMode ::Game -> Int -> IO Game
-randomMode sc@(Game mat score) player = do
-    dec@(r,c,e) <- randomDecision mat
+play :: GameState -> [(GameBoard -> IO Move)] -> Int -> IO GameState
+play sc@(Game mat score) players player = do
+    dec@(r,c,e) <- ((players !! player) $ mat)
     let mat2 = setElem e (r,c) mat
     let sc = doScore dec mat2
     let newScore = increaseScore score sc player
     if isFull mat2 then
         return (Game mat2 newScore)
     else if sc == 0 then
-        randomMode (Game (setElem e (r,c) mat2) newScore) (mod (player + 1) 2)
+        play (Game (setElem e (r,c) mat2) newScore) players (mod (player + 1) 2)
     else
-        randomMode (Game (setElem e (r,c) mat2) newScore) player
+        play (Game (setElem e (r,c) mat2) newScore) players player
 
 -- | The main entry point.
 main :: IO ()
@@ -84,6 +95,7 @@ main = do
     let c = (read s)::Int
     let mat = matrix r c (\_ -> '_')
     print mat
-    (Game res score) <- randomMode (Game mat (0,0)) 0
+    --(Game res score) <- randomMode (Game mat (0,0)) 0
+    (Game res score) <- play (Game mat (0,0)) [randomDecision,randomDecision] 0
     print res
     print score
